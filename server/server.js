303c5de4ef993a6f5bce68a0a58ad273e5bec182
farhan7479 <farhan078609@gmail.com>
2023-12-16 09:08:08 +0530

    f

--- server/server.js
+++ server/server.js
@@ -62,3 +62,13 @@ app.listen(PORT, () => {
       .white
   );
 });
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
