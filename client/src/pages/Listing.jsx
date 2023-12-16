63ba885bbf448fd3580ad7316b41c97503439bbd
farhan7479 <farhan078609@gmail.com>
2023-12-16 13:44:07 +0530

    f

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -90,10 +90,14 @@ export default function Listing() {
           )}
           <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
             <p className='text-2xl font-semibold'>
-              {listing.name} - ${' '}
+            {listing.name} - {' '}
               {listing.offer
-                ? listing.discountPrice.toLocaleString('en-US')
-                : listing.regularPrice.toLocaleString('en-US')}
+                ? listing.discountPrice.toLocaleString('en-IN', {
+                  style: 'currency',
+                  currency: 'INR',})
+                : listing.regularPrice.toLocaleString('en-IN', {
+                  style: 'currency',
+                  currency: 'INR',})}
               {listing.type === 'rent' && ' / month'}
             </p>
             <p className='flex items-center mt-6 gap-2 text-slate-600  text-sm'>
@@ -106,7 +110,7 @@ export default function Listing() {
               </p>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                  ${+listing.regularPrice - +listing.discountPrice} OFF
+                   ₹{+listing.regularPrice - +listing.discountPrice} OFF
                 </p>
               )}
             </div>
@@ -118,14 +122,14 @@ export default function Listing() {
               <li className='flex items-center gap-1 whitespace-nowrap '>
                 <FaBed className='text-lg' />
                 {listing.bedrooms > 1
-                  ? `${listing.bedrooms} beds `
-                  : `${listing.bedrooms} bed `}
+                  ? ` ${listing.bedrooms} bedrooms `
+                  : ` ${listing.bedrooms} bedroom `}
               </li>
               <li className='flex items-center gap-1 whitespace-nowrap '>
                 <FaBath className='text-lg' />
                 {listing.bathrooms > 1
-                  ? `${listing.bathrooms} baths `
-                  : `${listing.bathrooms} bath `}
+                  ? ` ${listing.bathrooms} bathrooms `
+                  : ` ${listing.bathrooms} bathroom `}
               </li>
               <li className='flex items-center gap-1 whitespace-nowrap '>
                 <FaParking className='text-lg' />
